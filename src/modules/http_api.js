c497539cfe929ce2f1d00561766cfd92cfe6dac4
H7-25 <simos@simosnap.org>
2022-04-01 00:51:55 +0200

    First http_api commit

--- src/modules/http_api.js
+++ src/modules/http_api.js
@@ -1,17 +1,26 @@
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
+const koaBody = require('koa-body');
+
+const ircPromises = require('../lib/ircPromises');
 
 module.exports = class HttpAPI {
     constructor(bot, config, channels, dbCon) {
         this.bot = bot;
         this.channels = channels;
+
+        this.inProgress = Object.create(null);
+
         this.koa = new Koa();
+
+        this.koa.use(koaBody());
+
         this.api = new KoaRouter({
             prefix: '/api',
         });
-        this.api.get('/get/nick', this.handleGetNick.bind(this));
-        this.api.get('/set/radio/:Chan/:radioname/:motd/:source/:icestats/:logo/:website/:twitch/:nowplay/:announce/:timer/:requests', this.handleSetRadio.bind(this));
-        this.api.get('/set/nick/:newNick', this.handleSetNick.bind(this));
+
+        this.api.post('/add/bot', this.handleAddBot.bind(this));
+        this.api.post('/set/radio', this.handleSetRadio.bind(this));
 
         this.koa.use(this.api.routes());
         this.koa.use(this.api.allowedMethods());
@@ -20,36 +29,50 @@ module.exports = class HttpAPI {
         this.koa.listen(3000);
     }
 
-    async handleGetNick(ctx) {
-        ctx.body = this.bot.user.nick || '';
-        console.log(this.channels);
+    async handleAddBot(ctx) {
+        // ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+        const newChan = ctx.request?.body?.channel;
+        const mbID = ctx.request?.body?.mbID;
+
+        if (this.inProgress.chanJoin) {
+            ctx.response.status = 500;
+            ctx.response.body = 'channel Join already in progress';
+            return;
+        }
+        this.inProgress.chanJoin = true;
+
+        const failValidation = (message) => {
+            delete this.inProgress.chanJoin;
+            ctx.throw(400, message);
+        };
+
+        if (!newChan) {
+            return failValidation('channel required');
+        }
+
+        if (!mbID) {
+            return failValidation('mbID required');
+        }
+
+        const chan = channels[newChan.toLowerCase()];
+
+        if (chan) {
+            return failValidation('channel already exist');
+        }
+
+        try {
+            const addedChan = await ircPromises.joinChan(this.bot, newChan, this.channels);
+            ctx.response.status = 200;
+            ctx.response.body = { nick: addedChan };
+        } catch (err) {
+            ctx.response.status = 500;
+            ctx.response.body = err;
+        }
+
+        delete this.inProgress.chanJoin;
     }
 
     async handleSetRadio(ctx) {
-        const Chan = ctx.params.Chan;
-        if (!Chan) {
-            return;
-        }
-
-        const chan = this.channels[Chan.toLowerCase()]; if (!chan) { console.error('i expected a channel object 1'); return; }
-        console.log(chan);
-        if (!chan) {
-            return;
-        }
-        if ((!chan.radioname) || (chan.radioname === '')) {
-            ctx.body = 'Radio non abilitata';
-        } else {
-            ctx.body = chan.name || '';
-        }
-        // ctx.body = chan.name || '';
-        // chan.dj = 'liamjnab';
-    }
-
-    async handleSetNick(ctx) {
-        const newNick = ctx.params.newNick;
-        if (!newNick) {
-            return;
-        }
-        this.bot.changeNick(newNick);
+        ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
     }
 };
