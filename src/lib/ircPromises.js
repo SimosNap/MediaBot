5b139c1ac223931be87303e6169e951b1977459e
H7-25 <simos@simosnap.org>
2022-04-01 13:52:33 +0200

    http_api

--- src/lib/ircPromises.js
+++ src/lib/ircPromises.js
@@ -4,43 +4,49 @@ exports.joinChan = function (bot, newChan, dbCon) {
         let removeListeners = null;
         let timeoutID = 0;
 
-        const chanJoin = (event) => {
-            removeListeners();
-            if (event.channel !== newChan) {
-                return reject('unexpected channel in join event');
+        const join = (event) => {
+            
+            if (!bot.caseCompare(event.channel, newChan.name)) {
+                // does not match the channel
+                return;
             }
-            resolve(chanJoin);
-        };
-
-        const cantJoin = (event) => {
+            if (!bot.caseCompare(event.nick, bot.user.nick)) {
+                // not not match the bot nick
+                return;
+            }
             removeListeners();
-            reject('join error');
+            resolve(event.channel);
+        };
+        
+        const error = (event) => {
+            removeListeners();
+            reject(`channel failed to join: ${event.error}`);
         };
 
         removeListeners = () => {
             clearTimeout(timeoutID);
-            bot.off('join', chanJoin);
-            bot.off('irc error', cantJoin);
-        };
+            bot.off('join', join);
+            bot.off('error', error);
+        }
 
         timeoutID = setTimeout(() => {
             removeListeners();
-            reject('nick change timed out');
+            reject('join timed out');
         }, 4000);
 
-        bot.on('join', chanJoin);
-        bot.on('nick in use', cantJoin);
+        bot.on('join', join);
+        bot.on('error', error);
 
-        dbCon.query('INSERT INTO magirc_mediabot_main (name) values (?)', [newChan], function (error, results, fields) {
+        dbCon.query('INSERT INTO magirc_mediabot_main (name) values (?)', [newChan.name], function (error, results, fields) {
             if (error) {
                 throw error;
             } else {
                 const mbID = results.insertId;
-                dbCon.query('INSERT INTO magirc_mediabot_youtube (id, channel, enabled) values (?,?,?)', [mbID, newChan, 1], function (error, results, fields) { if (error) throw error; });
-                dbCon.query('INSERT INTO magirc_mediabot_mixcloud (id, channel, enabled) values (?,?,?)', [mbID, newChan, 1], function (error, results, fields) { if (error) throw error; });
+                dbCon.query('INSERT INTO magirc_mediabot_youtube (id, channel, enabled) values (?,?,?)', [mbID, newChan.name, 1], function (error, results, fields) { if (error) throw error; });
+                dbCon.query('INSERT INTO magirc_mediabot_mixcloud (id, channel, enabled) values (?,?,?)', [mbID, newChan.name, 1], function (error, results, fields) { if (error) throw error; });
                 const obj = { mbID: mbID, radioname: '', motd: '', source: '', icestats: '', logo: '', website: '', twitch: '', nowplay: 0, announce: 0, timer: 0, requests: 0, youtube: 1, mixcloud: 1 };
                 Object.assign(newChan, obj);
-                bot.join(newChan);
+                bot.join(newChan.name);
             }
         });
         
