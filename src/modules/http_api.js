c0acf0da008f1a965c076b3b51b6ae8bed7d9203
H7-25 <simos@simosnap.org>
2022-04-06 12:46:10 +0200

    added debug

--- src/modules/http_api.js
+++ src/modules/http_api.js
@@ -355,6 +355,7 @@ module.exports = class HttpAPI {
                     this.bot.say(chan.name, '[ Adesso su ' + chan.radioname + ' ] ' + nowplaying + ' https://media.simosnap.com/player/' + chan.mbID, { '+simosnap.org/radio_stream': tagData.join(';') });
                 }, (60000 * 5));
                 this.bot.modules['radio.js'].playjobs[chan.name] = timeoutID;
+                console.log(this.bot.modules['radio.js'].playjobs[chan.name] );
             } else {
                 clearInterval(this.bot.modules['radio.js'].playjobs[chan.name]);
                 delete this.bot.modules['radio.js'].playjobs[chan.name];
