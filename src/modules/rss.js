a679bf626e069ceb301070e55e2505014f65a21c
H7-25 <simos@simosnap.org>
2022-04-27 02:13:22 +0200

    Forgotten rss devel interval ...

--- src/modules/rss.js
+++ src/modules/rss.js
@@ -46,7 +46,7 @@ module.exports = class rssnews {
 
         const channelfeed = {};
 
-        cron.schedule('*/1 * * * *', () => {
+        cron.schedule('*/15 * * * *', () => {
             (async() => {
                 for (const key in feeds) {
                     // console.log(`${key}: ${feeds[key].feed}`);
